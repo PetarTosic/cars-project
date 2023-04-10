e0b5e6e518e388d31d460f074518a0d5024e6efb
vivify <vivify@vivifys-MacBook-Pro-35.local>
2023-04-10 20:51:55 +0200

    Some things changed

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default')
+
+@section('content')
+  <h1 class="container">About Us</h1>
+@endsection
---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  @include('layout.heading')
+</head>
+<body>
+  @include('layout.navigation')
+
+  <main class="container mt-5 mb-5">
+    @yield('content')
+  </main>
+  
+  @include(('layout.footer'))
+</body>
+</html>
