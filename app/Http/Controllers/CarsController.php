b2d1f8e56d5d3acd395bb86d1a888fe915ea35f5
vivify <vivify@vivifys-MacBook-Pro-35.local>
2023-04-12 18:33:21 +0200

    Added cars view

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarsController extends Controller
+{
+    public function index() {
+        $cars = Car::all();
+        return view('/cars', compact('cars'));
+    }
+}
---
+++ resources/views/cars.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.default')
+
+@section('content')
+
+<h1 class="container">Cars list:</h1>
+
+@foreach ($cars as $car)
+<div class="row align-items-md-stretch">
+  <div class="col-md-6 mt-4">
+    <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+      <h2>Producer: {{$car->producer}}</h2>
+      <p>Model: {{$car->title}}</p>
+      <p>Number of doors: {{$car->number_of_doors}}</p>  
+    </div>
+  </div>
+</div>
+@endforeach
+
+@endsection
